9afe613d84f392eddf9df99f9cf0e459c6cafd73
Aoichaan0513 <aoichaan0513@gmail.com>
2020-03-08 04:22:07 +0900

    GitHub Actions の設定等 #9

--- app/electron/Firebase.js
+++ app/electron/Firebase.js
@@ -9,7 +9,7 @@ const pkg = require(`${app.getAppPath()}/package.json`);
 const protocolStr = 'flast';
 const fileProtocolStr = `${protocolStr}-file`;
 
-const cfg = require('./Config');
+// const cfg = require('./Config');
 
 const Config = require('electron-store');
 const config = new Config();
@@ -34,7 +34,16 @@ module.exports = class Firebase {
     constructor(defaultConfig) {
         this.defaultConfig = defaultConfig;
         if (!firebase.apps.length)
-            firebase.initializeApp(cfg.firebaseConfig);
+            firebase.initializeApp({
+                apiKey: process.env.FIREBASE_API_KEY,
+                appId: process.env.FIREBASE_APP_ID,
+                authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+                databaseURL: process.env.FIREBASE_DB_URL,
+                measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+                messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+            });
     }
 
     getDefaultConfig = () => {
@@ -337,7 +346,7 @@ module.exports = class Firebase {
             firebase.firestore().collection('users').doc(id).collection('bookmarks').where('url', '==', url).where('isPrivate', '==', isPrivate).get()
                 .then((items) => resolve(items.size > 0))
                 .catch((error) => {
-                    reject(err); 
+                    reject(err);
                     console.log(`Error: ${error} (isBookmarked)`)
                 });
         });
