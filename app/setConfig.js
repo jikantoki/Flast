2d651a79100d4006d8845d7f55c452290db3d660
Aoichaan0513 <aoichaan0513@gmail.com>
2020-03-10 23:13:16 +0900

    GitHub Actions の設定等 #16

--- app/setConfig.js
+++ app/setConfig.js
@@ -1,19 +1,19 @@
 const fs = require('fs');
 
 const json = JSON.stringify({
-    googleAPIKey: String(process.argv[2]),
+    googleAPIKey: String(process.env.GOOGLE_API_KEY),
     firebaseConfig: {
-        apiKey: String(process.argv[3]),
-        appId: String(process.argv[4]),
-        authDomain: String(process.argv[5]),
-        databaseURL: String(process.argv[6]),
-        measurementId: String(process.argv[7]),
-        messagingSenderId: String(process.argv[8]),
-        projectId: String(process.argv[9]),
-        storageBucket: String(process.argv[10])
+        apiKey: String(process.FIREBASE_API_KEY),
+        appId: String(process.env.FIREBASE_APP_ID),
+        authDomain: String(process.env.FIREBASE_AUTH_DOMAIN),
+        databaseURL: String(process.env.FIREBASE_DB_URL),
+        measurementId: String(process.env.FIREBASE_MEASUREMENT_ID),
+        messagingSenderId: String(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        projectId: String(process.env.FIREBASE_PROJECT_ID),
+        storageBucket: String(process.env.FIREBASE_STORAGE_BUCKET)
     }
 });
+console.log(process.env);
 console.log(process);
-console.log(process.argv);
 console.log(json);
 fs.writeFile('electron/Config.json', json, (err) => { });
